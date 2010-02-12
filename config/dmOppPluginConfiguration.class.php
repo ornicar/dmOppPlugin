2e01a3a876900d8e60286f034d01f8d69138906a
Thibault Duplessis <thibault.duplessis@gmail.com>
2010-02-12 15:07:13 +0100

    listened to dm.admin.menu event to move OPP menu to first position

--- config/dmOppPluginConfiguration.class.php
+++ config/dmOppPluginConfiguration.class.php
@@ -7,6 +7,13 @@ class dmOppPluginConfiguration extends sfPluginConfiguration
     $this->dispatcher->connect('dm.context.loaded', array($this,'listenToDmContextLoaded'));
 
     $this->dispatcher->connect('dm.content_chart.filter_modules', array($this, 'listenToContentChartFilterModulesEvent'));
+  
+    $this->dispatcher->connect('dm.admin.menu', array($this, 'listenToAdminMenuEvent'));
+  }
+
+  public function listenToAdminMenuEvent(sfEvent $e)
+  {
+    $e->getSubject()->getChild('OPP')->moveToFirst();
   }
 
   public function listenToDmContextLoaded(sfEvent $e)
